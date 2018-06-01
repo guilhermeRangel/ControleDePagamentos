2d9580cd8aa17f72cfe6121df6ac7cfa004a8fc2
guilhermeRangel <gui.vg16@hotmail.com>
2018-06-01 01:15:29 -0300

    Tratado erros de insercao de dados

--- api/billingCycle/billingCyclesServices.js
+++ api/billingCycle/billingCyclesServices.js
@@ -1,9 +1,32 @@
 //aqui é onde eu vou expor os servicos do tipo Rest
-
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']) //integra as rotas com Express
 BillingCycle.updateOptions({new: true, runValidatorns: true}) //sempre que for dar um update ele gera um obj new e o runValidatorns 
+
+
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next){
+    const bundle = res.locals.bundle
+
+    if(bundle.errors){
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
+
+function parseErrors(nodeRestfulErrors) {
+    const errors = []
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+    return errors
+}
+
+
+
 
 BillingCycle.route('count', function(req, res, next){
     BillingCycle.count(function(error, value){
