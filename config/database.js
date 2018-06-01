17ab86aa5207b581527f54c3ced1c1f9624750e2
guilhermeRangel <gui.vg16@hotmail.com>
2018-05-31 22:53:36 -0300

    Testado Insercao de dados no banco

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect("mongodb://localhost/db_finance", {useMongoClient: true});
-mongoose.Promise = global.Promise;
-
 mongoose.Error.messages.general.required = "O atributo'{PATH}' e obrigatorio"
