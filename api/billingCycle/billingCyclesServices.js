aee71ab155aec018d3bd786badd2612ca2858b76
guilhermeRangel <gui.vg16@hotmail.com>
2018-05-31 23:31:41 -0300

    Add msg de Erro

--- api/billingCycle/billingCyclesServices.js
+++ api/billingCycle/billingCyclesServices.js
@@ -3,5 +3,16 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']) //integra as rotas com Express
+BillingCycle.updateOptions({new: true, runValidatorns: true}) //sempre que for dar um update ele gera um obj new e o runValidatorns 
+
+BillingCycle.route('count', function(req, res, next){
+    BillingCycle.count(function(error, value){
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
 
 module.exports = BillingCycle
--- config/database.js
+++ config/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect("mongodb://localhost/db_finance", {useMongoClient: true});
 mongoose.Error.messages.general.required = "O atributo'{PATH}' e obrigatorio"
+
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite minimo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}'."
